388d01ffcd207c19386ef321908cb19a49c78f3e
Malik <hiraev@users.noreply.github.com>
2017-05-15 01:28:24 +0300

    Исчезающая платформа исчезает красиво!

--- src/classes/logic/platforms/VanishPlatform.java
+++ src/classes/logic/platforms/VanishPlatform.java
@@ -1,9 +1,15 @@
 package logic.platforms;
 
+import animations.OpacityTransition;
+import javafx.util.Duration;
+
 public class VanishPlatform extends Platform {
     private int gameWidth;
+    private OpacityTransition opacityTransition;
     public VanishPlatform(int gameWidth) {
         type = Type.Vanish;
+        opacityTransition = new OpacityTransition(Duration.seconds(1), this);
+        opacityTransition.setOnFinished(event -> setTranslateX(2 * gameWidth));
         this.gameWidth = gameWidth;
     }
 
@@ -14,6 +20,7 @@ public class VanishPlatform extends Platform {
 
     @Override
     public void touch() {
-        setTranslateX(2 * gameWidth);
+        opacityTransition.play();
+
     }
 }
