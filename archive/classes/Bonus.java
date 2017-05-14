b5c465275e35fcedcd79f7ebd3b3f88838b02205
Malik <hiraev@users.noreply.github.com>
2017-05-15 01:26:30 +0300

    ПРОЩАЙ МУСОР

--- archive/classes/Bonus.java
+++
@@ -1,5 +0,0 @@
-public class Bonus {
-    public enum Type {
-
-    }
-}
--- archive/classes/PlatformAnimation.java
+++
@@ -1,8 +0,0 @@
-import javafx.animation.Transition;
-
-public class PlatformAnimation extends Transition {
-    @Override
-    protected void interpolate(double frac) {
-
-    }
-}
