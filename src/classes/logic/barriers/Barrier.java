3db24adec0422864f546f45cc33d9872106eb167
Malik <hiraev@users.noreply.github.com>
2017-05-16 22:10:42 +0300

    Добавлены методы в класс Barrier

--- src/classes/logic/barriers/Barrier.java
+++ src/classes/logic/barriers/Barrier.java
@@ -3,4 +3,16 @@ package logic.barriers;
 import javafx.scene.shape.Rectangle;
 
 public abstract class Barrier extends Rectangle {
+    protected int windowWidth;
+    protected int width = 30;
+    protected int height  = 30;
+    public Barrier(int windowWidth) {
+        this.windowWidth = windowWidth;
+        setWidth(width);
+        setHeight(height);
+    }
+
+    public abstract void action();
+    public abstract void pause();
+    public abstract void continueAnimation();
 }
