b7c5a0e3ce681f5af77747a108ec48598ff4cf3f
Malik <hiraev@users.noreply.github.com>
2017-05-16 22:11:50 +0300

    Добавлены методы в класс Bonus

--- src/classes/logic/bonuses/Bonus.java
+++ src/classes/logic/bonuses/Bonus.java
@@ -3,4 +3,23 @@ package logic.bonuses;
 import javafx.scene.shape.Rectangle;
 
 public abstract class Bonus extends Rectangle {
+    protected static int windowWidth;
+    private static int size = 20;
+
+    public Bonus(int windowWidth) {
+        Bonus.windowWidth = windowWidth;
+        setWidth(size);
+        setHeight(size);
+    }
+
+    public abstract int getScore();
+    public abstract void pause();
+    public abstract void continueAnimation();
+
+    public void vanish(){
+        //Убираем бонус подальше
+        setTranslateX(3 * windowWidth);
+    }
+
+
 }
