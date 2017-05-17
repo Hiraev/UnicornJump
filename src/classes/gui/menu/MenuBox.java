ca58c8ed14046f73da464addd0c7ac3cef093269
Malik <hiraev@users.noreply.github.com>
2017-05-17 20:45:47 +0300

    Добавлен алгоритм определния и задания высоты

--- src/classes/gui/menu/MenuBox.java
+++ src/classes/gui/menu/MenuBox.java
@@ -1,13 +1,15 @@
 package gui.menu;
+
 import javafx.geometry.Pos;
 import javafx.scene.layout.VBox;
 
 public class MenuBox extends VBox {
-    public MenuBox(Button...buttons) {
+    public MenuBox(Button... buttons) {
         setAlignment(Pos.CENTER);
         setSpacing(10);
         getChildren().addAll(buttons);
-        setPrefSize(100,190);
+        setPrefSize(100, (buttons[0].getPrefHeight() + 10) * buttons.length);
         //setStyle("-fx-background-color: red;");
+        System.out.println(getPrefHeight());
     }
 }
