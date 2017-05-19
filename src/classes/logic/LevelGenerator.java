284521230051e5264a0479bde2a0800dc8559753
Malik <hiraev@users.noreply.github.com>
2017-05-19 22:52:25 +0300

    Три новых метода для интерфеса

--- src/classes/logic/LevelGenerator.java
+++ src/classes/logic/LevelGenerator.java
@@ -1,10 +1,13 @@
 package logic;
 
 public interface LevelGenerator {
-
-
-
     LevelMap updateLevel();
 
     LevelMap getLevel();
+
+    void levelDistributor();
+
+    void resetLastPlatformY();
+
+    int getLastPlatformY();
 }
