b79dfe39a27b239e97e9b3f4c46244962ce68a6e
Malik <hiraev@users.noreply.github.com>
2017-05-21 16:33:19 +0300

    Удален неиспользуемый конструктор и метод

--- src/classes/animations/OpacityTransition.java
+++ src/classes/animations/OpacityTransition.java
@@ -8,13 +8,6 @@ import javafx.util.Duration;
 public class OpacityTransition extends Transition {
     private Node node;
 
-    public void setNode(Node node) {
-        this.node = node;
-    }
-
-    public OpacityTransition() {
-
-    }
     public OpacityTransition(Duration duration, Node node) {
         this.node = node;
         setCycleDuration(duration);
